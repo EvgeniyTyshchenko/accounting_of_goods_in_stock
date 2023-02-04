0e7c1227f0965abe113eb7ca49102bd7856018f8
evgeniy_tyshchenko <tyshenko.evgeni@yandex.ru>
2023-02-04 15:12:24 +0300

    Initial commit

---
+++ src/main/java/ru/evgeniy/accountingofgoods/AccountingOfGoodsApplication.java
@@ -0,0 +1,13 @@
+package ru.evgeniy.accountingofgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingOfGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingOfGoodsApplication.class, args);
+    }
+
+}
